a2142a0ada66052b7a34e2011e3c517f93fccca1
Theo <jhn_hawley@hotmail.com>
2025-06-29 15:00:57 -0800

    added a bunch of stuff >,>

---
+++ functions/get_files_info.py
@@ -0,0 +1,19 @@
+import os
+
+
+
+def get_files_info(working_directory, directory=None):
+    work_path = os.path.join(working_directory, directory)
+    if not os.path.abspath(work_path).startswith(os.path.abspath(working_directory)):
+        return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if not os.path.isdir(work_path):
+        return(f'Error: "{directory}" is not a directory')
+    else:
+        names = os.listdir(work_path)
+        s_list = []
+        for name in names:
+            path = os.path.join(work_path, name)
+            size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            s_list.append(f"- {name}: file_size={size} bytes, is_dir={is_dir}")
+        return("\n".join(s_list))
--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+user_prompt = sys.argv[1]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]), ]
+
 if len(sys.argv) == 1:
     print("No prompt provided")
     sys.exit(1)
-else:
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+if "--verbose" in sys.argv:
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
+    print(f"User prompt: {user_prompt}")    
     print(response.text)
     print(f"Prompt tokens: {prompt_tokens}")
     print(f"Response tokens: {response_tokens}")
+else:
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    print(response.text)
