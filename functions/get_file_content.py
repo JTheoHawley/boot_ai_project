b84b304ab1b2ec7aa2cd0d7d4543834639fd74c9
Theo <jhn_hawley@hotmail.com>
2025-06-30 15:09:16 -0800

    updated to uv and added get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+
+
+
+def get_file_content(working_directory, file_path):
+    work_path = os.path.join(working_directory, file_path)
+    if not os.path.abspath(work_path).startswith(os.path.abspath(working_directory)):
+        return(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+    if not os.path.isfile(work_path):
+        return(f'Error: File not found or is not a regular file: "{file_path}"')
+    MAX_CHARS = 10000
+
+    try:
+        with open(work_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1) == (""):
+                return (file_content_string)
+            else:
+                return (file_content_string + f'[...File "{file_path}" truncated at 10000 characters]')
+    except Exception as e:
+        return f"Error: {e}"
+            
