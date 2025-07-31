61e593d440b520c5941350ad298e2979226b485f
Theo <jhn_hawley@hotmail.com>
2025-07-31 14:33:44 -0800

    Added function calling, and discovered that my code is fucked compared to the solution files -,-

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,5 @@
 import os
-
+from google.genai import types
 
 
 def get_files_info(working_directory, directory=None):
@@ -17,3 +17,18 @@ def get_files_info(working_directory, directory=None):
             is_dir = os.path.isdir(path)
             s_list.append(f"- {name}: file_size={size} bytes, is_dir={is_dir}")
         return("\n".join(s_list))
+    
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,31 +3,64 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 user_prompt = sys.argv[1]
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]), ]
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 
 if len(sys.argv) == 1:
     print("No prompt provided")
     sys.exit(1)
 if "--verbose" in sys.argv:
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
-    print(f"User prompt: {user_prompt}")    
-    print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
-else:
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages,
-                                               config=types.GenerateContentConfig(system_instruction=system_prompt))
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        print(f"User prompt: {user_prompt}")    
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(f"User prompt: {user_prompt}")    
+        print(response.text)
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
-    print(response.text)
+else:
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:    
+        print(response.text)
+
+
+
+
+
