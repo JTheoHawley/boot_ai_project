fb7efb26f6be83ac113d6a06c51db6aeb8c44078
Theo <jhn_hawley@hotmail.com>
2025-08-12 18:29:35 -0800

    added call_function function to main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file_content import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_file, run_python_file
+from functions.write_file_content import schema_write_file, write_file
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -37,9 +37,48 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+function_map = {"get_files_info": get_files_info,
+                "get_file_content": get_file_content,
+                "run_python_file":run_python_file,
+                "write_file": write_file,}
+
 config=types.GenerateContentConfig(
     tools=[available_functions], system_instruction=system_prompt
 )
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+    if verbose == True:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    if function_name in function_map:
+        function_args["working_directory"] = "./calculator"
+        actual_function = function_map[function_name]
+        function_result = actual_function(**function_args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+            )
+        ],
+    )
+
+
 
 if len(sys.argv) == 1:
     print("No prompt provided")
@@ -51,7 +90,13 @@ if "--verbose" in sys.argv:
 
     if response.function_calls:
         for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            function_call_result = call_function(function_call_part, verbose=True)
+            if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
+            ):
+                raise Exception("empty function call result")            
+            print(f"-> {function_call_result.parts[0].function_response.response}")
         print(f"User prompt: {user_prompt}")    
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
@@ -65,11 +110,15 @@ else:
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
     if response.function_calls:
         for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            function_call_result = call_function(function_call_part, verbose=False)
+            if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
+            ):
+                raise Exception("empty function call result")
+            print(f"-> {function_call_result.parts[0].function_response.response}")
     else:    
         print(response.text)
-
-
 
 
 
